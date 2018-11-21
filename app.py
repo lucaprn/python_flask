9fbe0e2e43c7a4363a8b7d84d0c78f807a934929
lucaprn <>
2018-11-21 17:31:50 +0100

    Initial functions flask (2)

--- app.py
+++ app.py
@@ -1,26 +1,27 @@
-from flask import Flask, request, redirect, make_response, abort
+from flask import Flask, request, redirect, render_template
+
 
 
 app = Flask(__name__)
+
+
 
 # pagina principale del sito
 
 @app.route('/')
 def hello_world():
-    return 'Ciao'
+    return render_template('index.html')
+
+# provo ad aprire una pagina creata
+@app.route("/prova")
+def prova():
+    return render_template('prova.html', title='Prova')
 
 @app.route('/user/<name>')
 def user(name):
     return '<h1> Hello %s </h1>' % name
 
-# pagina secondaria autogenerata
-
-@app.route('/prova')
-def prova():
-    return 'Prova'
-
 # request
-
 @app.route('/request')
 def index():
     user_agent = request.headers.get('User-Agent')
@@ -31,6 +32,12 @@ def index():
 @app.route('/reindirizza')
 def reindirizza():
     return redirect('https://www.google.it')
+
+# intercettazione errori
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 if __name__ == '__main__':
     app.run()
